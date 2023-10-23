8e5a5f7e63056a1f22f6b5de22346052a3533223
Karen C <kac23@ic.ac.uk>
2023-10-23 15:57:18 +0100

    app.py fixed spacing.

--- api/app.py
+++ api/app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template, send_file, request
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
-    
+
+
 @app.route("/Logo.png")
 def get_image():
-	return send_file("templates/Logo.png")
+    return send_file("templates/Logo.png")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     input_course = request.form.get("course")
-    return render_template("hello.html", name=input_name, age=input_age, course=input_course)
+    return render_template("hello.html", name=input_name,
+                           age=input_age, course=input_course)
