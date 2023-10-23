2fbd49ee34d0b137e08baf9f83233cb9233bc12c
Karen C <kac23@ic.ac.uk>
2023-10-23 17:22:23 +0100

    Adding remaining tests.

--- api/app.py
+++ api/app.py
@@ -23,9 +23,13 @@ def submit():
 
 
 @app.route("/query", methods=["GET"])
-def process_query():
+def query():
     q = request.args.get('q')
+    return process_query(q)
+
+
+def process_query(q):
     if q == 'dinosaurs':
-        return 'Successful'
+        return "Dinosaurs ruled the Earth 200 million years ago"
     else:
-        return 'Failure'
+        return 'Unknown'
