3ac5d08905a1df42e25eb7f61f8af9a36c1abe7c
Karen C <kac23@imperial.ac.uk>
2023-11-06 15:35:12 +0000

    'Adding a new page for GitHub username.'

--- api/app.py
+++ api/app.py
@@ -29,6 +29,13 @@ def query():
     q = request.args.get("q")
     return process_query(q)
 
+@app.route("/form")
+def form():
+    input_username = request.form.get("username")
+    return render_template(
+    "newpage.html", username=input_username
+    )
+
 
 def process_query(q):
     if q == "dinosaurs":
