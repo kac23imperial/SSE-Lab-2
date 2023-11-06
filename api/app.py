ba7a53a7bffbf1f06ba4dd5533136721dc10094c
Karen C <kac23@imperial.ac.uk>
2023-11-06 17:37:49 +0000

    'Formatting fixed.'

--- api/app.py
+++ api/app.py
@@ -49,14 +49,16 @@ def new_user():
 @app.route('/submit-request', methods=["POST"])
 def submit_username():
     username = request.form.get("username")
-    repos_response = requests.get(f"https://api.github.com/users/{username}/repos")
+    repos_response = requests.get(f"https://api.github.com/users/"
+                                  f"{username}/repos")
 
     if repos_response.status_code == 200:
         repos_data = repos_response.json()
         repo_commits = []
 
         for repo in repos_data:
-            commits_response = requests.get(repo['commits_url'].split('{')[0] + '?per_page=1')
+            commits_response = requests.get(repo['commits_url'].split('{')[0]
+                                            + '?per_page=1')
             if commits_response.status_code == 200:
                 commits_data = commits_response.json()
                 if commits_data:  # Check if there's at least one commit
@@ -64,16 +66,22 @@ def submit_username():
                     commit_info = {
                         'name': repo['name'],
                         'last_updated': repo['updated_at'],
-                        'latest_commit_hash': latest_commit['sha'],
-                        'latest_commit_author': latest_commit['commit']['author']['name'],
-                        'latest_commit_date': latest_commit['commit']['author']['date'],
-                        'latest_commit_message': latest_commit['commit']['message']
+                        'latest_commit_hash':
+                            latest_commit['sha'],
+                        'latest_commit_author':
+                            latest_commit['commit']['author']['name'],
+                        'latest_commit_date':
+                            latest_commit['commit']['author']['date'],
+                        'latest_commit_message':
+                            latest_commit['commit']['message']
                     }
                     repo_commits.append(commit_info)
-        return render_template("user_repos.html", username=username, repos=repo_commits)
+        return render_template("user_repos.html",
+                               username=username, repos=repo_commits)
     else:
 
-        error_message = f"Error fetching repositories. GitHub API returned status: {repos_response.status_code}"
+        error_message = (f"Error fetching repositories. GitHub API returned status:"
+                         f" {repos_response.status_code}")
         return render_template("error.html", error_message=error_message)
 
 
